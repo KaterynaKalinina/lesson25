b112279eb3dcad2e7e54cd91f16d829a1b28f12a
Kateryna Kalinina <katerynakalinina7@gmail.com>
2020-10-19 02:27:08 +0300

    axios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import Axios from "axios";
-import { curry, curryRight, last } from "lodash";
 import { drawData } from "./utils/dom";
 
 const apiUrl = 'http://localhost:3000';
@@ -7,7 +6,6 @@ const apiUrl = 'http://localhost:3000';
 const addBtn = document.getElementById('add'),
     deleteBtn = document.getElementById('delete'),
     wrapper = document.getElementById('data');
-
 
 
 Axios.get(apiUrl).then((response) => drawData(response.data, wrapper));
@@ -20,31 +18,12 @@ wrapper.addEventListener('click', (e) => {
 });
 
 addBtn.addEventListener('click', () => {
-    Axios.get(`${apiUrl}/add`).then((response) => drawData(response.data, wrapper));
-})
+    Axios.get(`${apiUrl}/add`)
+        .then((response) => drawData(response.data, wrapper));
+});
 
 deleteBtn.addEventListener('click', () => {
-    Axios.get(`${apiUrl}/delete`).then((response) => drawData(response.data, wrapper));
-})
-
-console.log(last([1,2,3]));
-
-const later = (value,cb, time = 1000) => setTimeout(() => cb(value), time);
-const promisifyLater = (value, time = 1000) => 
-    new Promise((res) => later(value, res, time));
-
-const firstTask = (value) => console.log(`First: ${value}`);
-const secondTask = (value) => console.log(`Second: ${value}`);
-
-const half = (value) => value / 2;
-const sqrt = (value) => Math.sqrt(value);
-
-const pr = promisifyLater(200);
-const halfPr = pr.then(half)
-                    .then(half)
-                    .then(curryRight(Math.pow)(2))
-                    .then(half)
-                    .then(half)
-                    .then(console.log);
-
+    Axios.get(`${apiUrl}/delete`)
+        .then((response) => drawData(response.data, wrapper));
+});
 
